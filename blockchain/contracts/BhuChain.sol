93f2360658a3a964277fa791a8e853dbb3b0d62c
Sulav Tamang <ta.sulav.man.singh22@ncmt.edu.np>
2026-01-20 15:58:20 +0545

    Add LandParcel structure and addParcel function

--- blockchain/contracts/BhuChain.sol
+++ blockchain/contracts/BhuChain.sol
@@ -9,5 +9,23 @@ contract BhuChain {
         uint256 area;
         address owner;
     }
-    
+    //2. Creating our "Digital Filing Cabinet" to store parcel
+    // We use the ID to find the parcel
+    mapping(uint256 => LandParcel) public parcels;
+
+    //3. Keep track of how many parcels we have
+    uint256 public parcelCount;
+
+    //This is just a placeholder to show the contract is working
+    string public name = "BhuChain Land Registry"; 
+
+    //4. Create a function to add a new parcel
+    function addParcel(string memory _location, uint256 _area) public {
+        //Increment the count(this will be our new Parcel ID)
+        parcelCount++;
+
+        //Create a new parcel and store it in our "cabinet"
+        //We use msg.sender to record WHO is adding the land
+        parcels[parcelCount] = LandParcel(parcelCount, _location, _area, msg.sender);
+    }
 }
