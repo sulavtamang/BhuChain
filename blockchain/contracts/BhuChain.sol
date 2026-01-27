771332977003c2bc7ce88124aceaac4740e51aaf
Sulav Tamang <ta.sulav.man.singh22@ncmt.edu.np>
2026-01-27 15:19:54 +0545

    Implement RBAC: Admin and Authorized Officer roles

--- blockchain/contracts/BhuChain.sol
+++ blockchain/contracts/BhuChain.sol
@@ -1,39 +1,47 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
 contract BhuChain {
-    //1. Defining what a "Land Parcel" looks like
+    address public admin;
+
     struct LandParcel {
         uint256 id;
         string location;
         uint256 area;
         address owner;
     }
-    //2. Creating our "Digital Filing Cabinet" to store parcel
-    // We use the ID to find the parcel
+
     mapping(uint256 => LandParcel) public parcels;
-
-    //3. Keep track of how many parcels we have
+    mapping(address => bool) public isOfficer; // List of authorized government officers
     uint256 public parcelCount;
+    string public name = "BhuChain Land Registry";
 
-    //This is just a placeholder to show the contract is working
-    string public name = "BhuChain Land Registry"; 
+    // Modifer: The "Security Guard" checkpoint
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only the admin can perform this action");
+        _;
+    }
 
-    //4. Create a function to add a new parcel
+    // Constructor: Runs only once when the contract is born
+    constructor() {
+        admin = msg.sender;
+        isOfficer[msg.sender] = true; // The admin is the first officer
+    }
+
+    // Function for the Admin to add new Land Revenue Officers
+    function addOfficer(address _officerAddress) public onlyAdmin {
+        isOfficer[_officerAddress] = true;
+    }
+
+    // Updated addParcel: Restricted to Authorized Officers only
     function addParcel(string memory _location, uint256 _area) public {
-        //Increment the count(this will be our new Parcel ID)
+        require(isOfficer[msg.sender], "Only authorized officers can register land");
         parcelCount++;
-
-        //Create a new parcel and store it in our "cabinet"
-        //We use msg.sender to record WHO is adding the land
         parcels[parcelCount] = LandParcel(parcelCount, _location, _area, msg.sender);
     }
-    //5. Function to transfer land from one address to another
-    function transferOwnership(uint256 _id, address _newOwner) public {
-        //Security: Ensure ONLY the current owner can transfer it
-        require(msg.sender == parcels[_id].owner, "You are not the owner of this parcel");
 
-        //Update the owner in our registry (Digital Filing Cabinet)
+    function transferOwnership(uint256 _id, address _newOwner) public {
+        require(msg.sender == parcels[_id].owner, "You are not the owner of this parcel");
         parcels[_id].owner = _newOwner;
     }
 }
