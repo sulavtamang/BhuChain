bead7efefa3a972152d115313f8789a4c4af576e
Sulav Tamang <ta.sulav.man.singh22@ncmt.edu.np>
2026-02-09 19:41:07 +0545

    feat(frontend): implement ProtectedRoute and setup React Router

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,24 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const ProtectedRoute = ({ requireOfficer = false }) => {
+    const { user, isOfficer, loading } = useAuth();
+
+    // 1. show loading while checkin auth
+    if (loading) return <div>Loading...</div>;
+
+    // 2. if not logged in -> Redirect to login
+    if (!user) {
+        return <Navigate to='/' replace />;
+    }
+
+    // 3. if officer role required but user connects as citizen -> redirect
+    if (requireOfficer && !isOfficer) {
+        return <Navigate to='dashboard' replace />;
+    }
+
+    // 4. if all good -> render the child route (outlet)
+    return <Outlet />;
+}
+
+export default ProtectedRoute;
