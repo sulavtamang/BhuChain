e610018c9aba2b8658884b0996e6185463f3f705
Sulav Tamang <ta.sulav.man.singh22@ncmt.edu.np>
2026-02-12 11:50:17 +0545

    feat(frontend): refactor landing page to add HeroSection component

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,7 +1,13 @@
 import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
 
 export default function LandingPage() {
     return (
-        <Navbar />
+        <div className="flex min-h-screen flex-col">
+            <Navbar />
+            <main className="flex-1">
+                <HeroSection />
+            </main>
+        </div>
     )
 };
